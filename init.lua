07828f2546873f66a2d031d7bf8f909b70ad9b33
Mehrain <marijnrovers@live.nl>
2024-07-20 20:10:45 +0200

    harpoon and copilot added

--- init.lua
+++ init.lua
@@ -219,7 +219,7 @@ vim.opt.rtp:prepend(lazypath)
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
---    :Lazy
+--    :Lazy:
 --
 --  You can press `?` in this menu for help. Use `:q` to close the window
 --
@@ -230,6 +230,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'ThePrimeagen/harpoon',
+  'github/copilot.vim',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
